8bcc89d49d00f564f8066d98c2098a92de7416a6
Robert Kenny <60153147+rlkenny98@users.noreply.github.com>
2021-02-20 18:51:09 -0800

    Implemented a login and logout system that displays the user's profile.

--- env/src/trydjango/trydjango/urls.py
+++ env/src/trydjango/trydjango/urls.py
@@ -16,13 +16,16 @@ Including another URLconf
 from django.urls import path, include
 
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from users import views as user_views
 from pages import views as pages_views
 
 urlpatterns = [
-
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
+    path('profile/', user_views.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('',pages_views.home_view,name='home'),
     path('contact/',pages_views.contact_view),
     path('about/',pages_views.about_view)
