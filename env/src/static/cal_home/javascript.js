4888f28a8bfb720264dbec63398ef0ab8c807378
esalapong <ejsalapong@csu.fullerton.edu>
2021-04-12 00:47:08 -0700

    added scrolling rotating quotes

--- env/src/static/cal_home/javascript.js
+++ env/src/static/cal_home/javascript.js
@@ -1,7 +1,10 @@
 var quotes = [
-  "It\'s only after you\'ve stepped outside your comfort zone that you begin to change, grow, and transform. \-Roy T. Bennett",
-  "Do what is right, not what is easy nor what is popular. \-Roy T. Bennett, The Light in the Heart",
-  "Believe in yourself. You are braver than you think, more talented than you know, and capable of more than you imagine. \-Roy T. Bennett, The Light in the Heart"
+  "<p>It\'s only after you\'ve stepped outside your comfort zone that you begin to change, grow, and transform. \-Roy T. Bennett</p>",
+  "<p>Do what is right, not what is easy nor what is popular. \-Roy T. Bennett, The Light in the Heart</p>",
+  "<p>Believe in yourself. You are braver than you think, more talented than you know, and capable of more than you imagine. \-Roy T. Bennett, The Light in the Heart</p>",
+  "<p>Be yourself; everyone else is already taken.\― Oscar Wilde</p>",
+  "<p>Be the change that you wish to see in the world.\― Mahatma Gandhi</p>",
+  "<p>Live as if you were to die tomorrow. Learn as if you were to live forever.\― Mahatma Gandhi</p>"
 ]
 
 function newQuote() {
